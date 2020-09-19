5d05151b26f9da52653092d389ebf0e200ba3326
sidharth <201952234@iiitvadodara.ac.in>
2020-09-19 20:17:40 +0530

    register redisgned

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Signin from './container/signin/Signin.js'
 import Register from './container/register/register.js'
 import Navbar from './container/Navbar/Navbar.js';
 import Landing from './container/Landing/Landing.js';
+import Login from './container/login/Login.js';
 
 
 function App() {
@@ -13,7 +14,7 @@ function App() {
 		<div>
 		<Route path="/" exact component={Navbar} />
     	<Route path="/" exact component={Landing} />
-    	<Route path="/signin" component={Signin} />
+    	<Route path="/signin" component={Login} />
     	<Route path="/register" component={Register} />
 	</div>
 
